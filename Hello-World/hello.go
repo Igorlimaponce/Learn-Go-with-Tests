82a5d510b4a22764a54913bd99c899503ac88f50
PC-ZUBU <igorlima0748@gmail.com>
2025-10-28 19:31:32 -0300

    mod - Hello-World (Complete)

--- Hello-World/hello.go
+++ Hello-World/hello.go
@@ -4,9 +4,27 @@ import "fmt"
 
 func main() {
 
-	fmt.Printf("%s", Hello("Joao"))
+	fmt.Printf("%s", Hello("Joao", "Spanish"))
 }
 
-func Hello(name string) string {
-	return fmt.Sprintf("Hello World! %s", name)
+const HelloPrefix = "Hello,"
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(name, language)
+}
+
+func greetingPrefix(name string, language string) string {
+	switch language {
+	case "Spanish":
+		return "Hola, " + name
+	case "French":
+		return "Bonjour, " + name
+	case "":
+		return fmt.Sprintf("%s %s", HelloPrefix, name)
+	default:
+		return fmt.Sprintf("%s %s %s", HelloPrefix, name, language)
+	}
 }
--- Hello-World/hello_test.go
+++ Hello-World/hello_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -17,21 +18,58 @@ import (
 // *** ***
 
 // Aqui vai dar um erro
-func TestHello(t *testing.T) {
-	got := Hello("Igor")
-	want := "Hello, World!!"
+var failNumber = 0
 
+func TestHello(t *testing.T) {
+	// Um dos beneficios do subteste é conseguir reaproveitar codigos
+	t.Run("Teste com Hello para Pessoas", func(t *testing.T) {
+		got := Hello("Igor", "")
+		want := "Hello, Igor"
+
+		if got != want {
+			t.Errorf("got = %q, want = %q", got, want)
+		}
+	})
+
+	t.Run("Teste com Hello para Pessoas com valor Vazio", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, Joao"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Em Espanhol", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Em frances", func(t *testing.T) {
+		got := Hello("Tom", "French")
+		want := "Bonjour, Tom"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+// Exemplo de reaproveitamento de codigo
+func assertCorrectMessage(t testing.TB, got string, want string) {
+	t.Helper()
+	// Ele ajuda no debug (O output do terminal retorna a linha em que a funcao que contem este Helper foi chamada (37))
+	// Caso eu comente o Helper a linha de retorno será a 47 (Oque é ruim para achar bugs)
 	if got != want {
 		t.Errorf("got = %q, want = %q", got, want)
 	}
 }
 
 // Aqui vai passar
+// Fiz esse teste primeiro antes de saber que dava para colocar subtestes (Por sinal é muito util)
 func TestHello2(t *testing.T) {
-	got := Hello("Joao")
-	want := "Hello World! Joao"
+	got := Hello("Joao", "")
+	want := "Hello, Joao"
 
 	if got != want {
+		failNumber += 1
+		fmt.Printf("FailNumber: %v\n", failNumber)
 		t.Errorf("got = %q, want = %q", got, want)
 	}
+
 }
