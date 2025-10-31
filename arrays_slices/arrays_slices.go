007b68c85ced6f813fd112d04d8932c28a867120
PC-ZUBU <igorlima0748@gmail.com>
2025-10-30 21:55:56 -0300

    mod - arrays_slices (Complete)

--- arrays_slices/arrays_slices.go
+++ arrays_slices/arrays_slices.go
@@ -1,5 +1,9 @@
 package arrays_slices
 
+import (
+	"fmt"
+)
+
 func SumArray(numbers [5]int) int {
 
 	var sum int
@@ -12,9 +16,9 @@ func SumArray(numbers [5]int) int {
 
 	// Esse for itera sobre o index
 	// ai no caso para somar voce passa a posicao
-	for i := range len(numbers) {
-		sum += numbers[i]
-	}
+	// for i := range len(numbers) {
+	// 	sum += numbers[i]
+	// }
 
 	return sum
 }
@@ -29,3 +33,64 @@ func SumSlices(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(n1 []int, n2 []int) []int {
+
+	var result []int
+	var sum1 int
+	var sum2 int
+
+	for _, i := range n1 {
+		sum1 += i
+	}
+
+	for _, i := range n2 {
+		sum2 += i
+	}
+
+	result = append(result, sum1, sum2)
+
+	return result
+
+}
+
+func SumAllAlternative(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = SumSlices(numbers)
+	}
+
+	fmt.Printf("numbersToSum = %v", numbersToSum)
+	return sums
+}
+
+func SumAllAlternative2(numbersToSum ...[]int) []int {
+	var sums []int
+	// numbers aqui vai ser um slice e vai passar pelo for com cada parte separada do numbersToSum	for _, numbers := range numbersToSum {
+	fmt.Printf("Tipo: %T\n", numbersToSum)
+	fmt.Printf("Quantidade de slices: %d\n", len(numbersToSum))
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, SumSlices(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(slices ...[]int) []int {
+	var sums []int
+	for _, i := range slices {
+		// Aqui ele pega a partir do primeiro numero
+		// Nesse caso o teste passa normalmente
+		i = i[1:]
+		sums = append(sums, SumSlices(i))
+	}
+
+	return sums
+}
+
+// func main() {
+// 	SumAllAlternative([]int{1, 2, 3, 4}, []int{8, 7, 6, 5})
+// }
--- arrays_slices/arrays_slices_test.go
+++ arrays_slices/arrays_slices_test.go
@@ -2,6 +2,7 @@ package arrays_slices
 
 import (
 	"fmt"
+	"slices"
 	"testing"
 )
 
@@ -29,6 +30,32 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+
+	t.Run("Teste com 2 Slices de Parametro", func(t *testing.T) {
+		// got := SumAll([]int{1, 2}, []int{0, 9})
+		// got := SumAllAlternative([]int{1, 2}, []int{0, 9})
+		got := SumAllAlternative2([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		// Tomar cuidado ao usar !reflect.DeepEqual
+		// Por mais que funcione para alguns casos ela nao bara comparacoes que nao sao do mesmo tipo
+		// Tornando perigoso para a aplicacao
+		// if !reflect.DeepEqual(got, want) {
+		// 	t.Errorf("got %v want %v", got, want)
+		// }
+
+		// Esse metodo foi adicionado recentemente e neste caso é melhor por
+		// ser focado em slices
+		CheckSums(t, got, want)
+
+	})
+
+	t.Run("Teste com a funcao TestSumAllTails, calcula a soma de todos os valores menos do primeiro", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{3, 4, 5})
+		want := []int{5, 9}
+
+		CheckSums(t, got, want)
+	})
 }
 
 func ExampleSumArray() {
@@ -43,4 +70,18 @@ func ExampleSumSlices() {
 	result := SumSlices(numbers)
 	fmt.Println(result)
 	// Output: 15
+}
+
+func CheckSums(t testing.TB, got, want []int) {
+	t.Helper()
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func CheckSums2(t testing.TB, got int, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
 }
